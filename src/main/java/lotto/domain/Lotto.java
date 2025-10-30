6965429737d9393208141eb1a956f253161a2397
gudwns1812 <gudwns1812@naver.com>
2025-10-30 19:19:14 +0900

    refactor: sort를 printer로 변경

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -17,8 +17,6 @@ public class Lotto {
     }
 
     public List<Integer> getNumbers() {
-        return numbers.stream()
-                .sorted()
-                .toList();
+        return numbers;
     }
 }
