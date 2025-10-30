7d8d8e3858b2e7c38f9474431880849f169fc9e7
gudwns1812 <gudwns1812@naver.com>
2025-10-30 19:20:07 +0900

    fix(LottoSeller): cash를 lotto_price로 나누도록 변경

--- src/main/java/lotto/domain/LottoSeller.java
+++ src/main/java/lotto/domain/LottoSeller.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import static lotto.domain.LottoLimitNumbers.LOTTO_PRICE;
+
 import java.util.List;
 import java.util.stream.IntStream;
 import lotto.domain.numbergenerator.NumberGenerator;
@@ -13,7 +15,8 @@ public class LottoSeller {
     }
 
     public List<Lotto> createLottoWithin(int cash) {
-        return IntStream.rangeClosed(0, cash)
+        int lottoCount = cash / LOTTO_PRICE.getValue();
+        return IntStream.range(0, lottoCount)
                 .mapToObj(i -> new Lotto(numberGenerator.generateNumbers()))
                 .toList();
     }
